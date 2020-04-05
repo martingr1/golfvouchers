9974b7773941f1f01da26de6add5de1619414016
martingr1 <martingarryrussell@gmail.com>
2020-04-05 15:07:55 +0000

    imported checkout app to project

---
+++ checkout/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from posts.models import Post
+
+
+class Order(models.Model):
+    first_name = models.CharField(max_length=50, blank=False)
+    last_name = models.CharField(max_length=50, blank=False)
+    contact_number = models.CharField(max_length=30, blank=False)
+    postcode = models.CharField(max_length=20, blank=True)
+    town_or_city = models.CharField(max_length=50, blank=False)
+    street_address_1 = models.CharField(max_length=50, blank=False)
+    street_address_2 = models.CharField(max_length=50, blank=False)
+    county = models.CharField(max_length=50, blank=False)
+    date = models.DateField()
+
+    def __str__(self):
+        return "{0}-{1}-{2}".format(self.id, self.date, self.first_name, self.last_name)
+
+
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False)
+    post = models.ForeignKey(Post, null=False)
+    quantity = models.IntegerField(blank=False)
+
+    def __str__(self):
+        return "{0} {1} @ {2}".format(self.quantity, self.post.title, self.post.price)
+
---
+++ checkout/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import OrderForm, MakePaymentForm
+from .models import OrderLineItem
+from django.conf import settings
+from posts.models import Post
+from django.utils import timezone
+import stripe
+
+stripe.api_key = settings.STRIPE_SECRET
+
+@login_required
+def checkout(request):
+    if request.method =='POST':
+        order_form = OrderForm(request.POST)
+        payment_form = MakePaymentForm(request.POST)
+
+        if order_form.is_valid() and payment_form.is_valid():
+            order = order_form.save(commit=False)
+            order.date = timezone.now()
+
+            cart = request.session.get('cart', {})
+            total = 0
+            for id, quantity in cart.items():
+                product = get_object_or_404(Post, pk=id)
+                total += quantity * product.price
+                order_line_item = OrderLineItem(
+                    order= order,
+                    product= product, 
+                    quantity= quantity)
+                order_line_item.save()
+            
+            try:
+                customer = stripe.Charge.create(
+                    amount=int(total * 100),
+                    currency="GBP",
+                    description=request.user.email,
+                    card=payment_form.cleaned_data['stripe_id'],
+                )
+            except stripe.error.CardError:
+                messages.error(request, "Your card has been declined")
+            
+            if customer.paid:
+                messages.error(request, "Transaction complete")
+                request.session['cart'] = {}
+                return redirect(reverse('get_posts'))
+
+            else:
+                messages.error(request, "Unable to take payment")
+        else: 
+            print(payment_form.errors)
+            messages.error(request, "Unable to take payment from card, please try another payment method")
+    else:
+        payment_form = MakePaymentForm()
+        order_form = OrderForm()
+    return render(request, "checkout.html", {'order_form':order_form, 'payment_form': payment_form,
+    'publishable': settings.STRIPE_PUBLISHABLE})

f04fdda1a0ad5867a50f7acebd3a82f587b3df85
martingr1 <martingarryrussell@gmail.com>
2020-04-27 11:05:07 +0000

    added additional styles for posts.html in static/js/scripts.js and css/custom.css

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,8 +1,19 @@
 $(document).ready(function() {
 
+    $('#view_filters').hide();
     $('#get_filters').on('click', function() {
-    
+
         $('#view_filters').slideToggle();
+
+    });
+    $('#posts').mouseenter(function() {
+
+        $('#posts').css('border-color', '#2F7320')
+    });
+
+    $('#posts').mouseleave(function() {
+        
+        $('#posts').css('border-color', '#FFFFFF')
 
     });
 
